5c85d5242efef614b680e4e797557a042fa67b80
Afrizaaa017 <afrizaharapan@gmail.com>
2026-06-10 07:32:57 +0700

    feat: add AdminOnly middleware to restrict access to admin users

---
+++ app/Http/Middleware/AdminOnly.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminOnly
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->role->is_full_access) {
+            return $next($request);
+        }
+
+        abort(403, 'Unauthorized - Admin access only');
+    }
+}
